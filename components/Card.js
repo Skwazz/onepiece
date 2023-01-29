b2d5895dcd1d1e394f6cee3c1c486e027bc3b589
Daniel Kamenkovich <>
2023-01-29 14:37:23 +0200

    Update cards to be fetch via getStaticProps

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,16 @@
 import Image from "next/image";
 const card = ({ id, alt }) => {
-  console.log(alt);
   return (
     <Image
       src={
-        alt == "-"
+        alt === "-"
           ? `/cards/${id}.png`
           : `/cards/${id}_${alt.toLowerCase()}.png`
       }
       alt="picture of a card"
-      width={150}
+      width={250}
       height={100}
+      className='hover:scale-105'
     />
   );
 };
--- pages/cardlist.js
+++ pages/cardlist.js
@@ -1,24 +1,12 @@
 import Card from "../components/Card";
 import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [cardsList, setCardsList] = useState([]);
+export default function Home({ cards }) {
 
-  useEffect(() => {
-    getCards();
-  }, []);
-
-  const getCards = async () => {
-    const res = await fetch("http://localhost:3000/api/cards");
-    const cards = await res.json();
-    return setCardsList(cards);
-  };
-
-  console.log(cardsList);
   return (
     <>
-      <div className="grid grid-cols-2 mx-auto md:grid-cols-6">
-        {cardsList?.map((card) => {
+      <div className="grid grid-cols-2 md:grid-cols-5 mx-auto ">
+        {cards?.map((card) => {
           if (card.name !== "-") {
             return <Card key={card._id} id={card.cardId} alt={card.altArt} />;
           }
@@ -26,4 +14,13 @@ export default function Home() {
       </div>
     </>
   );
+}
+export async function getStaticProps(context) {
+  const res = await fetch("http://localhost:3000/api/cards");
+  const cards = await res.json();
+  return {
+    props: {
+      cards,
+    },
+  }
 }
