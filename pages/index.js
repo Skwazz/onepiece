ce9173b9c27bf1a550181fc616f404e36c148ffb
Daniel Kamenkovich <>
2023-01-29 11:10:08 +0200

    Revert "Revert "Remove unused imports""
    This reverts commit 229ae50dcd3be82865c8c15e5caaaddf37d557e8.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import Navbar from "../components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
 
 export default function Home() {
   return (
