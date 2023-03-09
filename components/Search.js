d2d7eb0fbe4f31d7f9e06f600a6246c8c711ca5d
Skwazz <107321181+Skwazz@users.noreply.github.com>
2023-03-09 14:53:41 +0200

    Search quick fix

--- components/Search.js
+++ components/Search.js
@@ -1,21 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
-const Search = ({ setFilteredData, selectedColors, data }) => {
-  const filterBySearch = (input) => {
-    let updatedCards = [...data]; // need to use filtered array but it does not update when deleting, data does, find some solution.
-    updatedCards = updatedCards.filter((item) => {
-      return item.name.toLowerCase().indexOf(input.toLowerCase()) !== -1;
-    });
-    setFilteredData(updatedCards);
-  };
-
+const Search = ({ setSearch }) => {
   return (
     <div className="flex justify-center">
       <div className="flex justify-center">
         <input
           type="text"
-          onChange={(e) => filterBySearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           placeholder="Type To Search Cards!"
         />
       </div>
